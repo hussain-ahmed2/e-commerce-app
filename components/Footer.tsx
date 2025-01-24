da3102af45a47e5c190bb8231fbdaf0d4f89437f
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-01-24 22:00:10 +0600

    update footer change background color

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <footer className="border-t py-3 bg-neutral-600 text-white">
+        <footer className="border-t py-3 bg-neutral-800 text-white">
             <nav className="container mx-auto max-w-7xl">
                 <p className="text-center">&copy; 2021 My Store</p>
             </nav>
