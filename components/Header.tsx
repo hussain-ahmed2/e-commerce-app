a74cc25cfb73c70a13f4d633eaafe55d43dc7824
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-01-24 21:59:35 +0600

    update header set z-index 50

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Header() {
 	return (
-		<header className="fixed w-full border-b p-3 bg-emerald-600 text-white">
+		<header className="fixed w-full border-b p-3 bg-emerald-600 text-white z-50">
 			<nav className="container mx-auto max-w-7xl flex justify-between items-center">
 				<h1 className="text-2xl font-bold">My Store</h1>
 				<div className="space-x-8">
