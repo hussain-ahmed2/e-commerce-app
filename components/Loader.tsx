136c37d894315920aa5c966b83357248588a23fd
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-01-24 22:07:22 +0600

    create loader component for loading animation

---
+++ components/Loader.tsx
@@ -0,0 +1,5 @@
+export default function Loader() {
+    return <div className="py-16">
+        <div className="animate-spin border-8 border-b-amber-400 border-t-amber-400 h-20 w-20 rounded-full mx-auto"></div>
+    </div>
+}
