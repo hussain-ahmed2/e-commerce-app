d579c4e768ec1a8fc48ae040c7bf340a5e1cb77d
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-01-24 20:11:30 +0600

    add footer component

---
+++ components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <footer className="border-t py-3 bg-neutral-600 text-white">
+            <nav className="container mx-auto max-w-7xl">
+                <p className="text-center">&copy; 2021 My Store</p>
+            </nav>
+        </footer>
+    );
+}
