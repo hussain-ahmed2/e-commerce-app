6be8c7e7b50abe22643b5f5aa60debf3da54714b
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-01-24 20:11:08 +0600

    create header component

---
+++ components/Header.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+
+export default function Header() {
+	return (
+		<header className="fixed w-full border-b p-3 bg-emerald-600 text-white">
+			<nav className="container mx-auto max-w-7xl flex justify-between items-center">
+				<h1 className="text-2xl font-bold">My Store</h1>
+				<div className="space-x-8">
+                    <Link href="/">
+                        Home 
+                    </Link>
+                    <Link href="/products">
+                        Products
+                    </Link>
+                    <Link href='/products/categories'>
+                        Categories
+                    </Link>
+                </div>
+			</nav>
+		</header>
+	);
+}
