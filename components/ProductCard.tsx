dfcce11b39b874cd4675d62b50d72ed05620452d
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-01-25 22:03:55 +0600

    fix some styling to the card

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -3,22 +3,44 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function ProductCard({
-    id,
+	id,
 	thumbnail,
 	title,
 	price,
+	discountPercentage,
 	description,
 }: Product) {
+	const discountedPrice = price * (1 - discountPercentage / 100);
+
 	return (
-		<Link href={`/products/${id}`} className="shadow-md border rounded-md relative overflow-hidden">
+		<Link
+			href={`/products/${id}`}
+			className="shadow-md border rounded-md relative overflow-hidden group"
+		>
 			<div className="aspect-square">
-				<Image className="object-cover w-full" priority src={thumbnail} alt={title} height={300} width={300} />
+				<Image
+					className="object-cover w-full"
+					priority
+					src={thumbnail}
+					alt={`Image of ${title}`}
+					height={300}
+					width={300}
+				/>
 			</div>
 			<div className="px-4 pb-4">
-				<h3 className="font-bold text line-clamp-1 mt-2">{title}</h3>
+				<h3 className="font-bold text-lg line-clamp-1 mt-2">{title}</h3>
 				<p className="text-sm mt-2 line-clamp-2">{description}</p>
 			</div>
-			<h2 className="font-bold text-lg absolute -top-4 -left-1/2 mt-9 ml-10 bg-green-500 text-white py-2 w-full text-center -rotate-45 ">${price}</h2>
+			<div className="px-4 pb-4 mt-auto">
+				<div className="flex justify-between items-center">
+					<span className="font-bold text-lg text-green-500">
+						${discountedPrice.toFixed(2)}
+					</span>
+					<span className="font-bold text-sm text-gray-500 line-through">
+						${price.toFixed(2)}
+					</span>
+				</div>
+			</div>
 		</Link>
 	);
 }
