31e23dd7dfc646c29fa783b3ecedc200dd8401c8
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-01-26 11:12:56 +0600

    create search products component

---
+++ components/SearchProducts.tsx
@@ -0,0 +1,33 @@
+import { ProductListContext } from "@/context/ProductListContext";
+import { useContext } from "react";
+import { FaTimes } from "react-icons/fa";
+import { FaMagnifyingGlass } from "react-icons/fa6";
+
+export default function SearchProducts() {
+	const { searchTerm, setSearchTerm } = useContext(ProductListContext);
+
+	return (
+		<form className="flex items-center justify-center max-w-lg mx-auto container">
+			<div className="w-full relative">
+				<input
+					value={searchTerm}
+					onChange={(e) => setSearchTerm(e.target.value)}
+					className="p-2 rounded-s border w-full"
+					type="text"
+					name="search"
+					id="search"
+					placeholder="Search Products..."
+				/>
+				{searchTerm && (
+					<FaTimes onClick={() => setSearchTerm("")} className="absolute w-8 h-8 p-2 top-1/2 right-0 -translate-x-1 -translate-y-1/2 cursor-pointer hover:bg-gray-200 rounded-full" />
+				)}
+			</div>
+			<button
+				className=" border hover:bg-gray-200 p-3 rounded-e"
+				type="submit"
+			>
+				<FaMagnifyingGlass />
+			</button>
+		</form>
+	);
+}
