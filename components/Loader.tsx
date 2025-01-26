84c819585e355034bea4164004a448eca33ba0f8
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-01-26 14:40:54 +0600

    change loader color

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -1,5 +1,5 @@
 export default function Loader() {
     return <div className="py-16">
-        <div className="animate-spin border-8 border-b-amber-400 border-t-amber-400 h-20 w-20 rounded-full mx-auto"></div>
+        <div className="animate-spin border-8 border-b-emerald-400 border-t-emerald-400 h-20 w-20 rounded-full mx-auto"></div>
     </div>
 }
