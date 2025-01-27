d16c5bdce0af7b28bb5885ffe3aa6c9a93d11026
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-01-27 17:13:12 +0600

    update footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
     return (
         <footer className="border-t py-3 bg-neutral-800 text-white">
             <nav className="container mx-auto max-w-7xl">
-                <p className="text-center">&copy; 2021 My Store</p>
+                <p className="text-center">&copy; {new Date().getFullYear()} My Store</p>
             </nav>
         </footer>
     );
